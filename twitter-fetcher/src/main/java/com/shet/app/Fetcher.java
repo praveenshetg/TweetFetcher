63e8138f84c9b7b96b304f59fb274f341752ebca
unknown <quotationmaker.com>
2017-12-12 04:59:48 +0530

    first commit

---
+++ twitter-fetcher/src/main/java/com/shet/app/Fetcher.java
@@ -0,0 +1,20 @@
+//package com.shet.app;
+//
+//import org.springframework.social.connect.ConnectionRepository;
+//import org.springframework.social.twitter.api.Twitter;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/")
+//public class Fetcher {
+//
+//	private Twitter twitter;
+//
+//	private ConnectionRepository connectionRepository;
+//
+//	public Fetcher(Twitter twitter, ConnectionRepository connectionRepository) {
+//		this.twitter = twitter;
+//		this.connectionRepository = connectionRepository;
+//	}
+//}
